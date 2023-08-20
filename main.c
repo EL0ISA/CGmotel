4003958c27f373b0f400fb038096c227bde4a9d1
Eloisa <maria-eloisa-santos@outlook.com>
2023-08-19 22:26:38 -0300

    telas de clientes e funcionarios

--- main.c
+++ main.c
@@ -5,11 +5,57 @@ void inicio(void);
 void sobre(void);
 void menu_principal(void);
 
+void menu_clientes(void);
+void cad_cli(void);
+void pesq_cli(void);
+void edit_cli(void);
+void del_cli(void);
+
+void menu_funcionarios(void);
+void cad_func(void);
+void pesq_func(void);
+void edit_func(void);
+void del_func(void);
+
 int main(void) {
     inicio();
     sobre();
     menu_principal();
+    menu_clientes();
+    cad_cli();
+    pesq_cli();
+    edit_cli();
+    del_cli();
+    menu_funcionarios();
+    cad_func();
+    pesq_func();
+    edit_func();
+    del_func();
     return 0;
+}
+
+void inicio(void){
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|                 Universidade Federal do Rio Grande do Norte                   |\n");
+    printf("|                     Centro de Ensino Superior do Seridó                       |\n");
+    printf("|                   Departamento de Computação e Tecnologia                     |\n");
+    printf("|                      Disciplina DCT1106 -- Programação                        |\n");
+    printf("|                     Projeto Sistema de Gestão de Motel                        |\n");
+    printf("|               Desenvolvido por Mª Eloisa da Silva Santos (@EL0ISA)            |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+}
+void sobre(void){
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|                               Sobre o projeto                                 |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("| O CGmotel é um projeto desenvolvido para a disciplina de programação com fins |\n");
+    printf("| de aprendizagem. O mesmo consiste em um sistema de gestão para um motel, dessa|\n");
+    printf("| forma contando com os modulos de clientes, quartos, reservas e funcionarios.  |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|                           Desenvolvido por Eloisa                             |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
 void menu_principal(void){
     char opc;
@@ -27,26 +73,125 @@ void menu_principal(void){
     scanf("%c",&opc);
     getchar();
 }
-void sobre(void){
+
+void menu_clientes(void){
+    char opc;
+    system("clear||cls");
     printf("*-------------------------------------------------------------------------------*\n");
-    printf("|                               Sobre o projeto                                 |\n");
+    printf("*                               Menu Clientes                                   *\n");
     printf("*-------------------------------------------------------------------------------*\n");
-    printf("| O CGmotel é um projeto desenvolvido para a disciplina de programação com fins |\n");
-    printf("| de aprendizagem. O mesmo consiste em um sistema de gestão para um motel, dessa|\n");
-    printf("| forma contando com os modulos de clientes, quartos, reservas e funcionarios.  |\n");
+    printf("|                             1. Cadastrar novo                                 |\n");
+    printf("|                             2. Pesquisar dados                                |\n");
+    printf("|                             3. Atualizar dados                                |\n");
+    printf("|                             4. Deletar cliente                                |\n");
+    printf("|                             0. Voltar                                         |\n");
     printf("*-------------------------------------------------------------------------------*\n");
-    printf("|                           Desenvolvido por Eloisa                             |\n");
+    printf("-- Sua opc: ");
+    scanf("%c",&opc);
+    getchar();
+}
+void cad_cli(void){
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("                 .......   Cadastrando novo cliente   .......                    \n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - CPF (apenas números):                                            |\n");
+    printf("|            - Nome completo:                                                   |\n");
+    printf("|            - E-mail:                                                          |\n");
+    printf("|            - Data de Nascimento (dd/mm/aaaa):                                 |\n");
     printf("*-------------------------------------------------------------------------------*\n");
     printf("\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
-void inicio(void){
+void pesq_cli(void){
+    system("clear||cls");
     printf("*-------------------------------------------------------------------------------*\n");
-    printf("|                 Universidade Federal do Rio Grande do Norte                   |\n");
-    printf("|                     Centro de Ensino Superior do Seridó                       |\n");
-    printf("|                   Departamento de Computação e Tecnologia                     |\n");
-    printf("|                      Disciplina DCT1106 -- Programação                        |\n");
-    printf("|                     Projeto Sistema de Gestão de Motel                        |\n");
-    printf("|               Desenvolvido por Mª Eloisa da Silva Santos (@EL0ISA)            |\n");
+    printf("                 .......   Pesquisando dados de cliente   .......                \n");
     printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - Informe o CPF (apenas números):                                  |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
+void edit_cli(void){
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("                 .......   Atualizando dados de cliente   .......                \n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - Informe o CPF (apenas números):                                  |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+void del_cli(void){
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("                 .......   Deletando cliente   .......                           \n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - Informe o CPF (apenas números):                                  |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void menu_funcionarios(void){
+    char opc;
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("*                               Menu Funcionarios                               *\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|                             1. Cadastrar novo                                 |\n");
+    printf("|                             2. Pesquisar dados                                |\n");
+    printf("|                             3. Atualizar dados                                |\n");
+    printf("|                             4. Deletar funcionario                            |\n");
+    printf("|                             0. Voltar                                         |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("-- Sua opc: ");
+    scanf("%c",&opc);
+    getchar();
+}
+void cad_func(void){
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("                 .......   Cadastrando novo funcionario   .......                \n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - CPF (apenas números):                                            |\n");
+    printf("|            - Nome completo:                                                   |\n");
+    printf("|            - E-mail:                                                          |\n");
+    printf("|            - Data de Nascimento (dd/mm/aaaa):                                 |\n");
+    printf("|            - Função:                                                          |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+void pesq_func(void){
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("                 .......   Pesquisando dados de funcionario   .......            \n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - Informe o CPF (apenas números):                                  |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+void edit_func(void){
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("                 .......   Atualizando dados de funcionario   .......            \n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - Informe o CPF (apenas números):                                  |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+void del_func(void){
+    system("clear||cls");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("                 .......   Deletando funcionario   .......                       \n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("|            - Informe o CPF (apenas números):                                  |\n");
+    printf("*-------------------------------------------------------------------------------*\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
